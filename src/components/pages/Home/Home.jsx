4a961128d78bcdf36dd832cac720efef5e542a42
mosinmiloluwa <mosinmiloluwa.o@gmail.com>
2023-10-14 22:12:56 +0100

    new changes

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -3,14 +3,12 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import ContactUs from "./components/ContactUs/ContactUs";
 import WelcomeSection from "./components/WelcomeSection/WelcomeSection";
 import classes from "./Home.module.scss";
-import FirstTimer from "./components/FirstTimer/FirstTimer";
 
 function Home() {
   return (
     <div className={classes.homeContainer}>
       <WelcomeSection />
       <AboutUs />
-      {/* <FirstTimer /> */}
       <ContactUs />
       <Footer />
     </div>
